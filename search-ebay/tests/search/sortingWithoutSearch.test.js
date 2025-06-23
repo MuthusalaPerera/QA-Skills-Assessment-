47f3e27b7d6afa779eb0aea6966731a5b8e9d601
Pawani Perera <89269937+MuthusalaPerera@users.noreply.github.com>
2025-06-23 11:07:01 +0530

    Update sortingWithoutSearch.test.js

--- search-ebay/tests/search/sortingWithoutSearch.test.js
+++ search-ebay/tests/search/sortingWithoutSearch.test.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('TC020 - Sorting without input does nothing', async ({ page }) => {
   await page.goto('https://www.ebay.com');
   const sortSelect = await page.$('select[aria-label="Sort"]');
-  expect(sortSelect).toBeNull(); // There’s no sort dropdown unless search is performed
+  expect(sortSelect).toBeNull(); 
 });
